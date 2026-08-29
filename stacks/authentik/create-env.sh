bd4762f83eb820f5bf22f50fe4c71a633509744f
doxy <doxy.desu@gmail.com>
2026-08-29 16:42:14 -0700

    add authentik email settings

--- stacks/authentik/create-env.sh
+++ stacks/authentik/create-env.sh
@@ -1,2 +1,3 @@
 echo "PG_PASS=$(openssl rand -base64 36 | tr -d '\n')" >> .env
 echo "AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')" >> .env
+echo "AUTHENTIK_EMAIL__PASSWORD=REPLACE ME" >> .env
